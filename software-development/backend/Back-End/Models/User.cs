1ce3dfd80e3e2eb2d1563ce0a9ad99179351716c
andy <61367661+chanandy7@users.noreply.github.com>
2023-08-06 23:26:23 +1200

    deleted backend folder
    backend is in its own separate repo

--- software-development/backend/Back-End/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Back_End.Models
-{
-    public class User
-    {
-        public string Id { get ; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }    
-}
--- software-development/backend/Back-End/Services/IUserService.cs
+++
@@ -1,9 +0,0 @@
-using Back_End.Models;
-
-namespace Back_End.Services
-{
-    public interface IUserService
-    {
-        Task<User> Authenticate(string username, string password);
-    }
-}
